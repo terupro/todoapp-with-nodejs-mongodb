df217f250f94f4eed629e62ddd332d2dc292e67b
‘terupro’ <terupro911@gmail.com>
2022-09-06 16:12:44 +0900

    ✨ MongoDBとの接続

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require("express");
 const app = express();
 const taskRoute = require("./routes/tasks");
 const PORT = 4000;
+const connectDB = require("./db/connect");
 
+// ルーティング設計
 app.use("/api/tasks", taskRoute);
 
-app.listen(PORT, console.log("サーバーが起動しました"));
+// データベースと接続
+const start = async () => {
+  try {
+    await connectDB(
+      "mongodb+srv://terupro:terupro1914911M@cluster0.yzomryk.mongodb.net/todoapp?retryWrites=true&w=majority"
+    );
+    app.listen(PORT, console.log("サーバーが起動しました"));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
